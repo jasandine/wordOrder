eef03ee3cf0cba434cb9d276be02e51abab8cb2f
David Zulaica and Joey Sandine <jasandine@gmail.com>
2015-02-12 11:12:15 -0800

    Setup project dir

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+var wordOrder = function(string) {
+
+  var stringArr = string.split(" ");
+  // var counter = stringArr.length;
+  var uniqueWords = [];
+
+  uniqueWords = uniqueWords.push(stringArr);
+
+  return uniqueWords;
+
+}
---
+++ spec/specs.js
@@ -0,0 +1,7 @@
+describe("wordOrder", function() {
+
+  it("puts a word into an array", function() {
+    expect(wordOrder("this")).to.eql(["this"]);
+  });
+
+});
