0b65d346a5746e8252e9658baa35aeb8388a83c4
David Zulaica and Joey Sandine <zulaica@gmail.com>
2015-02-12 15:39:14 -0800

    Notes

--- js/scripts.js
+++ js/scripts.js
@@ -2,21 +2,53 @@ var wordOrder = function(string) {
 
   var words = string.toLowerCase().split(" ");
   var uniqueWords = [];
-  var uniqueWordOccurance = [];
+  var uniqueWordOccurrence = [];
+  var sortedWords = [];
 
   words.forEach(function(word) {
     if ( uniqueWords.indexOf(word) === -1 ) {
       uniqueWords.push(word);
-      uniqueWordOccurance.push(1);
+      uniqueWordOccurrence.push(1);
     } else {
-      // console.log("Word: " + word);
       var position = uniqueWords.indexOf(word);
-      // console.log("Word position: " + position);
-      // console.log("Unique word occurance before: " + uniqueWordOccurance[position]);
-      uniqueWordOccurance[position]++;
-      // console.log("Unique word occurance after: " + uniqueWordOccurance[position]);
+      uniqueWordOccurrence[position]++;
+      // // var highestOccurrence = uniqueWordOccurrence
+      //
+      // if ( uniqueWordOccurrence > uniqueWordOccurrence ) {
+      //   uniqueWords = uniqueWords.sort();
+      // }
     }
   });
 
+//   var temporarySort = uniqueWordOccurrence.sort().reverse();
+//   // console.log("----------");
+//   // console.log("Temp Sort: " + temporarySort);
+//
+//   temporarySort.forEach(function(occurrence) {
+// debugger;
+//     var the_index = uniqueWordOccurrence.indexOf(occurrence);
+//     // console.log("The Index: " + the_index);
+//   });
+
   return uniqueWords;
 }
+
+
+
+// uniqueWords = ["one","two","three"]
+// uniqueWordOccurrence = [1,2,3]
+//
+// temporarySort [3,2,1].reverse
+//
+// uniqueWordOccurrence.indexOf(temporarySort[each])
+//
+// the index is 3
+//
+// sortedWords.push(uniqueWords[3])
+//
+// pop all
+//
+// loop through until done.
+//
+
+// [["one", 1], ["two", 2], ["three", 3]]
